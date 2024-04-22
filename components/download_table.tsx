a3994965c5eeb47b1cd37c320804ca47ef56283b
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-22 11:17:34 +0800

    feat(ui): colorize download button

--- components/download_table.tsx
+++ components/download_table.tsx
@@ -86,6 +86,7 @@ const DownloadTableComponent = ({ molecules }: MoleculeTableProps) => {
                                 onClick={(event) =>
                                     handleDownload(molecule.cas_id, event)
                                 }
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                             >
                                 Download
                             </button>
