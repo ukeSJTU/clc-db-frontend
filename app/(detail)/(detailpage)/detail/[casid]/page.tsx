5784de74160bb5c3a73e3a4cc08bb77078f35fde
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-22 10:32:08 +0800

    chore: add todo list for detail page
    Later, a scroll bar should be implemented to indicate which section the viewer is.

--- app/(detail)/(detailpage)/detail/[casid]/page.tsx
+++ app/(detail)/(detailpage)/detail/[casid]/page.tsx
@@ -30,7 +30,9 @@ const MoleculeDetailPage = ({ params }: { params: { casid: string } }) => {
         <div className="container mx-auto px-4 py-8">
             {molecule.length > 0 && <MoleculeDetailSheet {...molecule[0]} />}
             <div className="flex">
-                <div className="w-1/4">{/* Scroll Bar will go here */}</div>
+                <div className="w-1/4">
+                    {/* TODO: Scroll Bar will go here */}
+                </div>
             </div>
         </div>
     );
