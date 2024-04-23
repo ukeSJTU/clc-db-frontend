53aa56900727654fedb9a74b7738db39faf073b0
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-23 11:16:47 +0800

    feat: :sparkles: add callback function for perform search

--- app/(search)/search/page.tsx
+++ app/(search)/search/page.tsx
@@ -5,7 +5,10 @@ import axios from "axios";
 
 import SearchBarComponent from "@/components/searchbar";
 import MoleculeCard from "@/components/molecule_card";
-import { completeMoleculeProps } from "@/types/molecule";
+import {
+    completeMoleculeProps,
+    simplifiedMoleculeProps,
+} from "@/types/molecule";
 
 import SearchHeading from "@/components/searchpage/heading";
 import SearchBar from "@/components/searchpage/bar";
@@ -19,7 +22,7 @@ const SearchPage = () => {
     const options = [{ name: "CAS ID" }, { name: "Name" }, { name: "SMILE" }];
 
     const handleSearch = async () => {
-        if (query === "") {
+        if (query.trim() === "") {
             return; // Do not search if query is empty
         }
 
@@ -38,7 +41,11 @@ const SearchPage = () => {
         <div className="flex flex-col items-center py-12 space-y-4">
             <SearchHeading />
             <div className="flex flex-col gap-2 w-full max-w-md sm:max-w-lg md:max-w-2xl">
-                <SearchBar />
+                <SearchBar
+                    query={query}
+                    setQuery={setQuery}
+                    handleSearch={handleSearch}
+                />
                 <SearchTip />
                 <div className="flex items-center gap-4">
                     {options.map((option, index) => (
@@ -49,7 +56,13 @@ const SearchPage = () => {
             <div
                 id="search-result"
                 className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto p-4"
-            ></div>
+            >
+                {results.map(
+                    (molecule: simplifiedMoleculeProps, index: number) => (
+                        <MoleculeCard key={index} {...molecule} />
+                    )
+                )}
+            </div>
         </div>
     );
 };
