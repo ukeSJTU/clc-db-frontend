64f3f18716d328e396a8aba8e51bbe2db0e5ebd1
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-23 10:46:12 +0800

    feat(ui): :sparkles: add search tip component

---
+++ components/searchpage/tip.tsx
@@ -0,0 +1,9 @@
+const SearchTip = () => {
+    return (
+        <small className="text-xs text-gray-500 dark:text-gray-400">
+            Tip: Tick multiple options to search by multiple fields.
+        </small>
+    );
+};
+
+export default SearchTip;
