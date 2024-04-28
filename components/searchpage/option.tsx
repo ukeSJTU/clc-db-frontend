ce2b661515f9dc3a23f6602cd73f9d330d1285fd
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-29 00:49:11 +0800

    feat(search): :sparkles: add callback func for search option to setup dynamic search url

--- components/searchpage/option.tsx
+++ components/searchpage/option.tsx
@@ -3,15 +3,31 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 
 interface SearchOptionProps {
-    name: string;
+    displayName: string;
+    searchName: string;
+    isChecked: boolean;
+    onChange: (name: string) => void;
     icon?: string;
 }
 
-const SearchOption: React.FC<SearchOptionProps> = ({ name, icon }) => {
+const SearchOption: React.FC<SearchOptionProps> = ({
+    displayName,
+    searchName,
+    isChecked,
+    onChange,
+}) => {
+    const handleChange = (checked: boolean) => {
+        onChange(searchName);
+    };
+
     return (
         <div className="flex items-center gap-2">
-            <Checkbox id={name} />
-            <Label htmlFor={name}>{name}</Label>
+            <Checkbox
+                id={searchName}
+                checked={isChecked}
+                onCheckedChange={handleChange}
+            />
+            <Label htmlFor={searchName}>{displayName}</Label>
 
             {/* {icon && <img src={icon} alt={name} />} */}
         </div>
