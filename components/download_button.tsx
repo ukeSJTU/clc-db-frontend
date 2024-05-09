b6495522e2a438d0af86f0fa59c9807811d17c92
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-09 15:05:19 +0800

    feat(ui): :sparkles: add a download button

--- components/download_button.tsx
+++ components/download_button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import {
     Select,
     SelectContent,
@@ -9,8 +8,10 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 import downloadFiles from "@/lib/download";
 import { MoleculeProps } from "@/types/molecule";
+import { DownloadIcon } from "lucide-react";
 
 interface DownloadButtonProps {
     molecules: MoleculeProps[];
@@ -21,20 +22,20 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
     molecules,
     sdfFiles,
 }) => {
-    const [downloadOption, setDownloadOption] = useState("csv");
+    const [downloadOption, setDownloadOption] = useState("zip");
 
     const handleDownload = () => {
         downloadFiles(downloadOption, molecules, sdfFiles);
     };
 
     return (
-        <div className="flex gap-2 items-center">
+        <div className="flex flex-row gap-2 items-center">
             <Select
                 onValueChange={(value) => setDownloadOption(value)}
-                defaultValue="csv"
+                defaultValue="zip"
             >
-                <SelectTrigger className="w-32">
-                    <SelectValue placeholder="CSV" />
+                <SelectTrigger>
+                    <SelectValue placeholder="Download Both (ZIP)" />
                 </SelectTrigger>
                 <SelectContent>
                     <SelectItem value="csv">CSV</SelectItem>
@@ -42,7 +43,10 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
                     <SelectItem value="zip">Both (ZIP)</SelectItem>
                 </SelectContent>
             </Select>
-            <Button onClick={handleDownload}>Download</Button>
+            {/* Add a button to manually trigger the download */}
+            <Button onClick={handleDownload} title="Download">
+                <DownloadIcon />
+            </Button>
         </div>
     );
 };
