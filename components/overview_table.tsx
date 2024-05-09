8c97b5e55f3771d97baa80e26d3f127d2783d1bd
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-09 08:20:39 +0800

    feat(ui): :sparkles: truncate long molecule name with ellipsis

--- components/overview_table.tsx
+++ components/overview_table.tsx
@@ -18,54 +18,56 @@ const MoleculeTable = ({ molecules }: MoleculeTableProps) => {
     };
 
     return (
-        <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-                <tr>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Name
-                    </th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        CAS ID
-                    </th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Class Type
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200 ">
-                {molecules.map((molecule, index) => (
-                    <tr
-                        key={index}
-                        onClick={() => handleClick(molecule.cas_id)}
-                        className="hover:bg-gray-300/10"
-                    >
-                        <td className="px-6 py-4 whitespace-nowrap">
-                            {molecule.name}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                            {molecule.cas_id}
-                        </td>
-                        {/* <td className="px-6 py-4 whitespace-nowrap">
-                            {molecule.class_type.length > 0
-                                ? molecule.class_type[0].name
-                                : "None"}
-                        </td> */}
-                        <td className="px-6 py-4 whitespace-nowrap">
-                            {molecule.class_type.length > 0 ? (
-                                molecule.class_type.map((type, idx) => (
-                                    <ClassTypeBadge
-                                        key={idx}
-                                        classType={type}
-                                    />
-                                ))
-                            ) : (
-                                <ClassTypeBadge classType={{ name: "None" }} />
-                            )}
-                        </td>
+        <div className="overflow-auto">
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                    <tr>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Name
+                        </th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            CAS ID
+                        </th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Class Type
+                        </th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                    {molecules.map((molecule, index) => (
+                        <tr
+                            key={index}
+                            onClick={() => handleClick(molecule.cas_id)}
+                            className="hover:bg-gray-300/10 cursor-pointer"
+                        >
+                            {/* Name column with ellipsis if too long */}
+                            <td className="px-6 py-4 max-w-xs overflow-hidden text-ellipsis whitespace-nowrap">
+                                {molecule.name}
+                            </td>
+                            {/* CAS ID column without truncation */}
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                {molecule.cas_id}
+                            </td>
+                            {/* Class Type column with ellipsis */}
+                            <td className="px-6 py-4 max-w-xs overflow-hidden text-ellipsis whitespace-nowrap">
+                                {molecule.class_type.length > 0 ? (
+                                    molecule.class_type.map((type, idx) => (
+                                        <ClassTypeBadge
+                                            key={idx}
+                                            classType={type}
+                                        />
+                                    ))
+                                ) : (
+                                    <ClassTypeBadge
+                                        classType={{ name: "None" }}
+                                    />
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
