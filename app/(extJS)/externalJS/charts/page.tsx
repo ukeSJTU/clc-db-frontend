07fec8508b1af74ab27192cfe629448d58d768ee
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-10 20:46:54 +0800

    feat(stats): :sparkles: import weight distribution chart to chartpage

--- app/(extJS)/externalJS/charts/page.tsx
+++ app/(extJS)/externalJS/charts/page.tsx
@@ -1,8 +1,11 @@
+import WeightDistributionChart from "@/components/charts/WeightDistributionChart";
+
 export default function Page() {
     return (
         <div>
             <h1>Charts</h1>
             <p>This is the Charts page.</p>
+            <WeightDistributionChart />
         </div>
     );
 }
