0d0bceb268cd9fb8eb616db9544cf3f4b717cbb8
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-12 18:49:50 +0800

    feat: :sparkles: update the backend url setting

--- utils/api.tsx
+++ utils/api.tsx
@@ -2,11 +2,11 @@
 import axios from "axios";
 
 // Set base URL from environment variable
-const API_BASE_URL = process.env.NEXT_PUBLIC_APP_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_DOMAIN;
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL + "/api",
 });
 
 export default api;
