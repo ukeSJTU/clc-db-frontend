a9f77b36bc631704df1d2051e158092132cf1a06
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-14 11:23:42 +0800

    feat(ui): :sparkles: rebuild the ui for descriptor params component

--- components/clusterpage/DescriptorParams.tsx
+++ components/clusterpage/DescriptorParams.tsx
@@ -10,6 +10,7 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Control, Controller } from "react-hook-form";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 interface E3FPParametersProps {
     control: Control<any>;
@@ -26,161 +27,268 @@ interface DescriptorParametersProps {
 
 const E3FPParameters: React.FC<E3FPParametersProps> = ({ control }) => {
     return (
-        <div className="flex flex-row">
-            <FormField
-                control={control}
-                name="bits"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>nBits of RDKit</FormLabel>
-                        <FormControl>
-                            <Input
-                                type="number"
-                                // min={0}
-                                step={24}
-                                value={field.value}
-                                onChange={(
-                                    e: React.ChangeEvent<HTMLInputElement>
-                                ) => field.onChange(Number(e.target.value))}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="radius"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Radius multiplier</FormLabel>
-                        <FormControl>
-                            <Slider min={0} max={10} step={0.1} {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="rdkitInv"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Feature</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onChange={(
-                                    e: React.ChangeEvent<HTMLInputElement>
-                                ) => field.onChange(Boolean(e.target.value))}
-                                defaultValue={field.value}
-                            >
-                                <RadioGroupItem value="Yes">Yes</RadioGroupItem>
-                                <RadioGroupItem value="No">No</RadioGroupItem>
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-        </div>
+        <Card>
+            <CardHeader>
+                <CardTitle>E3FP Parameters</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-col space-y-4">
+                    <FormField
+                        control={control}
+                        name="bits"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>nBits of RDKit</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="number"
+                                        // min={0}
+                                        step={24}
+                                        value={field.value}
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLInputElement>
+                                        ) =>
+                                            field.onChange(
+                                                Number(e.target.value)
+                                            )
+                                        }
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="radius"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Radius multiplier</FormLabel>
+                                <FormControl>
+                                    <Slider
+                                        min={0}
+                                        max={10}
+                                        step={0.1}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitInv"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Feature</FormLabel>
+                                <FormControl>
+                                    <RadioGroup
+                                        onChange={(
+                                            e: React.ChangeEvent<HTMLInputElement>
+                                        ) =>
+                                            field.onChange(
+                                                Boolean(e.target.value)
+                                            )
+                                        }
+                                        defaultValue={field.value}
+                                    >
+                                        <RadioGroupItem value="Yes">
+                                            Yes
+                                        </RadioGroupItem>
+                                        <RadioGroupItem value="No">
+                                            No
+                                        </RadioGroupItem>
+                                    </RadioGroup>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </CardContent>
+        </Card>
     );
 };
 
 const RDKitParameters: React.FC<RDKitParametersProps> = ({ control }) => {
     return (
-        <div className="flex flex-row">
-            <FormField
-                control={control}
-                name="bits"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>nBits of RDKit</FormLabel>
-                        <FormControl>
-                            <Input
-                                type="number"
-                                // min={0}
-                                step={24}
-                                value={field.value}
-                                onChange={(e) =>
-                                    field.onChange(Number(e.target.value))
-                                }
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="rdkitRadius"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Radius</FormLabel>
-                        <FormControl>
-                            <Slider min={0} max={10} step={1} {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="rdkitUseFeatures"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>useFeatures</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onValueChange={field.onChange}
-                                defaultValue={field.value ? "Yes" : "No"}
-                            >
-                                <RadioGroupItem value="Yes">Yes</RadioGroupItem>
-                                <RadioGroupItem value="No">No</RadioGroupItem>
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="rdkitUseBondTypes"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>useBondTypes</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onValueChange={field.onChange}
-                                defaultValue={field.value ? "Yes" : "No"}
-                            >
-                                <RadioGroupItem value="Yes">Yes</RadioGroupItem>
-                                <RadioGroupItem value="No">No</RadioGroupItem>
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={control}
-                name="rdkitUseChirality"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>useChirality</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onValueChange={field.onChange}
-                                defaultValue={field.value ? "Yes" : "No"}
-                            >
-                                <RadioGroupItem value="Yes">Yes</RadioGroupItem>
-                                <RadioGroupItem value="No">No</RadioGroupItem>
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-        </div>
+        <Card>
+            <CardHeader>
+                <CardTitle>RDKit Parameters</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-col space-y-4">
+                    <FormField
+                        control={control}
+                        name="bits"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>nBits of RDKit</FormLabel>
+                                {/* ... (Input component) */}
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitRadius"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Radius</FormLabel>
+                                {/* ... (Slider component) */}
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseFeatures"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useFeatures</FormLabel>
+                                {/* ... (RadioGroup component) */}
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseBondTypes"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useBondTypes</FormLabel>
+                                {/* ... (RadioGroup component) */}
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseChirality"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useChirality</FormLabel>
+                                {/* ... (RadioGroup component) */}
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={control}
+                        name="bits"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>nBits of RDKit</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="number"
+                                        // min={0}
+                                        step={24}
+                                        value={field.value}
+                                        onChange={(e) =>
+                                            field.onChange(
+                                                Number(e.target.value)
+                                            )
+                                        }
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitRadius"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Radius</FormLabel>
+                                <FormControl>
+                                    <Slider
+                                        min={0}
+                                        max={10}
+                                        step={1}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseFeatures"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useFeatures</FormLabel>
+                                <FormControl>
+                                    <RadioGroup
+                                        onValueChange={field.onChange}
+                                        defaultValue={
+                                            field.value ? "Yes" : "No"
+                                        }
+                                    >
+                                        <RadioGroupItem value="Yes">
+                                            Yes
+                                        </RadioGroupItem>
+                                        <RadioGroupItem value="No">
+                                            No
+                                        </RadioGroupItem>
+                                    </RadioGroup>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseBondTypes"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useBondTypes</FormLabel>
+                                <FormControl>
+                                    <RadioGroup
+                                        onValueChange={field.onChange}
+                                        defaultValue={
+                                            field.value ? "Yes" : "No"
+                                        }
+                                    >
+                                        <RadioGroupItem value="Yes">
+                                            Yes
+                                        </RadioGroupItem>
+                                        <RadioGroupItem value="No">
+                                            No
+                                        </RadioGroupItem>
+                                    </RadioGroup>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="rdkitUseChirality"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>useChirality</FormLabel>
+                                <FormControl>
+                                    <RadioGroup
+                                        onValueChange={field.onChange}
+                                        defaultValue={
+                                            field.value ? "Yes" : "No"
+                                        }
+                                    >
+                                        <RadioGroupItem value="Yes">
+                                            Yes
+                                        </RadioGroupItem>
+                                        <RadioGroupItem value="No">
+                                            No
+                                        </RadioGroupItem>
+                                    </RadioGroup>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </CardContent>
+        </Card>
     );
 };
 
