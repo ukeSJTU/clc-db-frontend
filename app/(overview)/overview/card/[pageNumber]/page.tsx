84a1a6c3fe663b4a8fb483bec0a633cd83a39c19
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-14 10:34:58 +0800

    feat(ui): :sparkles: use new container for overview page

--- app/(overview)/overview/card/[pageNumber]/page.tsx
+++ app/(overview)/overview/card/[pageNumber]/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { PaginationComponent } from "@/components/Pagination";
 import { useState, useEffect } from "react";
 import api from "@/utils/api";
 import OverviewContainer from "@/components/OverviewContainer";
@@ -27,59 +25,66 @@ const fetchMoleculeData = async (
 const IndexPage = ({ params }: { params: { pageNumber: string } }) => {
     const pageNumber = parseInt(params.pageNumber, 10);
     const [molecules, setMolecules] = useState([]);
-    const [page, setPage] = useState(pageNumber);
-    const [pageSize, setPageSize] = useState(10); // Default page size
-    const [totalPages, setTotalPages] = useState(0); // Initially set to 0
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [paginationState, setPaginationState] = useState({
+        page: pageNumber,
+        pageSize: 10,
+        totalPages: 0,
+    });
 
     useEffect(() => {
         const fetchData = async () => {
             const { results, totalPages } = await fetchMoleculeData(
-                page,
-                pageSize,
+                paginationState.page,
+                paginationState.pageSize,
                 selectedCategory
             );
             setMolecules(results);
-            setTotalPages(totalPages);
+            setPaginationState((prevState) => ({ ...prevState, totalPages }));
         };
         fetchData();
-    }, [page, pageSize, selectedCategory]);
+    }, [paginationState.page, paginationState.pageSize, selectedCategory]);
 
     const handleCategoryChange = (category: string) => {
         setSelectedCategory(category);
-        setPage(1); // Reset to the first page when changing the category
+        setPaginationState((prevState) => ({ ...prevState, page: 1 })); // Reset to the first page when changing the category
+    };
+
+    const handlePaginationChange = (
+        newPaginationState: typeof paginationState
+    ) => {
+        setPaginationState(newPaginationState);
     };
 
     return (
         <div className="max-w-6xl mx-auto p-4">
             {/* Header with centered Title */}
-            <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-200 text-center mb-4">
+            <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-200 text-center mb-8">
                 Molecule Overview
             </h2>
-
-            <div className="flex justify-between items-center mb-4">
-                {/* Left: Category Selector */}
-                <div className="flex-1 pr-8">
+            {/* Overview Container for results */}
+            <OverviewContainer
+                molecules={molecules}
+                useLayoutSwitch={false}
+                paginationProps={{
+                    page: paginationState.page,
+                    setPage: (page) =>
+                        handlePaginationChange({ ...paginationState, page }),
+                    pageSize: paginationState.pageSize,
+                    setPageSize: (pageSize) =>
+                        handlePaginationChange({
+                            ...paginationState,
+                            pageSize,
+                        }),
+                    totalPages: paginationState.totalPages,
+                }}
+                topLeftComponent={
                     <CategorySelector
                         selectedCategory={selectedCategory}
                         onCategoryChange={handleCategoryChange}
                     />
-                </div>
-
-                {/* Right: Pagination Component */}
-                <div className="flex-1 pl-8">
-                    <PaginationComponent
-                        page={page}
-                        setPage={setPage}
-                        pageSize={pageSize}
-                        setPageSize={setPageSize}
-                        totalPages={totalPages}
-                    />
-                </div>
-            </div>
-
-            {/* Overview Container for results */}
-            <OverviewContainer molecules={molecules} useLayoutSwitch={false} />
+                }
+            />
         </div>
     );
 };
