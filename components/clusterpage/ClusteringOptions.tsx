a5c2abcfa96858e5df2fb027b4a663ca7ec33b1c
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-14 11:20:36 +0800

    feat(ui): :sparkles: rebuild the cluster option component

--- components/clusterpage/ClusteringOptions.tsx
+++ components/clusterpage/ClusteringOptions.tsx
@@ -16,6 +16,7 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Control, Controller } from "react-hook-form";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 interface ClusteringOptionsProps {
     control: Control<any>;
@@ -31,195 +32,220 @@ const ClusteringOptions: React.FC<ClusteringOptionsProps> = ({
     clusterMethod,
 }) => {
     return (
-        <>
-            <h3>Choose the style you cluster</h3>
-            <h5>Dimension Reduction Methods</h5>
-            <ul>
-                <li>
-                    <strong>TSNE</strong>: Powerful but time consuming
-                </li>
-                <li>
-                    <strong>PCA</strong>: Balanced between performance and time
-                </li>
-            </ul>
-            <FormField
-                control={control}
-                name="reductionMethod"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Embedding Methods</FormLabel>
-                        <Select
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                        >
-                            <FormControl>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select an embedding method" />
-                                </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                                <SelectItem value="PCA">PCA</SelectItem>
-                                <SelectItem value="TSNE">TSNE</SelectItem>
-                            </SelectContent>
-                        </Select>
-                        {field.value === "TSNE" && bits > fileListLength && (
-                            <FormMessage className="text-red-500">
-                                Error! When using TSNE, the number of files must
-                                be greater than the dimension of descriptors.
-                            </FormMessage>
-                        )}
-                        {field.value === "TSNE" && (
-                            <FormMessage>
-                                Dimension of descriptors:{" "}
-                                <strong>{bits}</strong>, Number of input files:{" "}
-                                <strong>{fileListLength}</strong>
-                            </FormMessage>
-                        )}
-                    </FormItem>
+        <Card className="mb-4">
+            <CardHeader>
+                <CardTitle>Clustering Options</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="mb-4">
+                    <h4 className="text-lg font-semibold mb-2">
+                        Dimension Reduction Methods
+                    </h4>
+                    <ul className="list-disc list-inside">
+                        <li>
+                            <strong>TSNE</strong>: Powerful but time consuming
+                        </li>
+                        <li>
+                            <strong>PCA</strong>: Balanced between performance
+                            and time
+                        </li>
+                    </ul>
+                </div>
+                <FormField
+                    control={control}
+                    name="reductionMethod"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Embedding Methods</FormLabel>
+                            <Select
+                                onValueChange={field.onChange}
+                                defaultValue={field.value}
+                            >
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select an embedding method" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    <SelectItem value="PCA">PCA</SelectItem>
+                                    <SelectItem value="TSNE">TSNE</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            {field.value === "TSNE" &&
+                                bits > fileListLength && (
+                                    <FormMessage className="text-red-500">
+                                        Error! When using TSNE, the number of
+                                        files must be greater than the dimension
+                                        of descriptors.
+                                    </FormMessage>
+                                )}
+                            {field.value === "TSNE" && (
+                                <FormMessage>
+                                    Dimension of descriptors:{" "}
+                                    <strong>{bits}</strong>, Number of input
+                                    files: <strong>{fileListLength}</strong>
+                                </FormMessage>
+                            )}
+                        </FormItem>
+                    )}
+                />
+
+                <div className="mb-4">
+                    <h4 className="text-lg font-semibold mb-2">
+                        Cluster Methods Choice
+                    </h4>
+                    <ul className="list-disc list-inside">
+                        <li>
+                            <strong>KNN</strong>: Good method
+                        </li>
+                        <li>
+                            <strong>DBSCAN</strong>
+                        </li>
+                    </ul>
+                </div>
+                <FormField
+                    control={control}
+                    name="clusterMethod"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Cluster Methods</FormLabel>
+                            <Select
+                                onValueChange={field.onChange}
+                                defaultValue={field.value}
+                            >
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select a cluster method" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    <SelectItem value="KNN">KNN</SelectItem>
+                                    <SelectItem value="DBSCAN">
+                                        DBSCAN
+                                    </SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </FormItem>
+                    )}
+                />
+
+                {clusterMethod === "KNN" && (
+                    <Card className="mt-4">
+                        <CardHeader>
+                            <CardTitle>KNN Parameters</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <FormField
+                                control={control}
+                                name="clusters"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Num of clusters</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="number"
+                                                min={2}
+                                                max={
+                                                    fileListLength === 0
+                                                        ? 10
+                                                        : fileListLength - 1
+                                                }
+                                                value={field.value}
+                                                onChange={(e) =>
+                                                    field.onChange(
+                                                        Number(e.target.value)
+                                                    )
+                                                }
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={control}
+                                name="knnAlgro"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Algorithm</FormLabel>
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select an algorithm" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="lloyd">
+                                                    lloyd
+                                                </SelectItem>
+                                                <SelectItem value="elkan">
+                                                    elkan
+                                                </SelectItem>
+                                                <SelectItem value="auto">
+                                                    auto
+                                                </SelectItem>
+                                                <SelectItem value="full">
+                                                    full
+                                                </SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                        </CardContent>
+                    </Card>
                 )}
-            />
 
-            <h5>Cluster Methods Choice</h5>
-            <ul>
-                <li>
-                    <strong>KNN</strong>: Good method
-                </li>
-                <li>
-                    <strong>DBSCAN</strong>
-                </li>
-            </ul>
-            <FormField
-                control={control}
-                name="clusterMethod"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Cluster Methods</FormLabel>
-                        <Select
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                        >
-                            <FormControl>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select a cluster method" />
-                                </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                                <SelectItem value="KNN">KNN</SelectItem>
-                                <SelectItem value="DBSCAN">DBSCAN</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </FormItem>
+                {clusterMethod === "DBSCAN" && (
+                    <Card className="mt-4">
+                        <CardHeader>
+                            <CardTitle>DBSCAN Parameters</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <FormField
+                                control={control}
+                                name="eps"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>eps</FormLabel>
+                                        <FormControl>
+                                            <Slider
+                                                min={0}
+                                                max={2}
+                                                step={0.01}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={control}
+                                name="minSamples"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>min_samples</FormLabel>
+                                        <FormControl>
+                                            <Slider
+                                                min={1}
+                                                max={10}
+                                                step={1}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </CardContent>
+                    </Card>
                 )}
-            />
-
-            {clusterMethod === "KNN" && (
-                <>
-                    <p>Additional parameters required by KNN:</p>
-                    <FormField
-                        control={control}
-                        name="clusters"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Num of clusters</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="number"
-                                        min={2}
-                                        max={
-                                            fileListLength === 0
-                                                ? 10
-                                                : fileListLength - 1
-                                        }
-                                        value={field.value}
-                                        onChange={(e) =>
-                                            field.onChange(
-                                                Number(e.target.value)
-                                            )
-                                        }
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={control}
-                        name="knnAlgro"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Algorithm</FormLabel>
-                                <Select
-                                    onValueChange={field.onChange}
-                                    defaultValue={field.value}
-                                >
-                                    <FormControl>
-                                        <SelectTrigger>
-                                            <SelectValue placeholder="Select an algorithm" />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        <SelectItem value="lloyd">
-                                            lloyd
-                                        </SelectItem>
-                                        <SelectItem value="elkan">
-                                            elkan
-                                        </SelectItem>
-                                        <SelectItem value="auto">
-                                            auto
-                                        </SelectItem>
-                                        <SelectItem value="full">
-                                            full
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </FormItem>
-                        )}
-                    />
-                </>
-            )}
-
-            {clusterMethod === "DBSCAN" && (
-                <>
-                    <p>Additional parameters required by DBSCAN:</p>
-                    <FormField
-                        control={control}
-                        name="eps"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>eps</FormLabel>
-                                <FormControl>
-                                    <Slider
-                                        min={0}
-                                        max={2}
-                                        step={0.01}
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={control}
-                        name="minSamples"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>min_samples</FormLabel>
-                                <FormControl>
-                                    <Slider
-                                        min={1}
-                                        max={10}
-                                        step={1}
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </>
-            )}
-        </>
+            </CardContent>
+        </Card>
     );
 };
 
