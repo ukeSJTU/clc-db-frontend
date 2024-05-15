e60d3f76d29069786c7ccbb9f23c807ee1613673
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-15 23:46:08 +0800

    feat(ui): update ui of stats page for better readability

--- app/(statistics)/statistics/page.tsx
+++ app/(statistics)/statistics/page.tsx
@@ -9,22 +9,42 @@ const StatisticsPage = () => {
     return (
         <div className="mx-auto p-4 mt-8 space-y-6">
             <h2 className="text-2xl font-semibold">Charts</h2>
-            <h3 className="text-xl font-semibold">
-                Molecule Distribution by Weight
-            </h3>
-            <WeightDistributionChart />
-            <h3 className="text-xl font-semibold">
-                Molecule Distribution by Category
-            </h3>
-            <CategoryDistChart />
-            <h3 className="text-xl font-semibold">
-                Molecule Distribution by Chirality
-            </h3>
-            <ChiralityDistChart />
-            <h3 className="text-xl font-semibold">
-                Example Result for Cluster
-            </h3>
-            <ClusterDemoChart />
+
+            <div className="w-full md:w-2/3 lg:w-1/2 mx-auto">
+                <h3 className="text-xl font-semibold">
+                    Molecule Distribution by Weight
+                </h3>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                    <WeightDistributionChart />
+                </div>
+            </div>
+
+            <div className="w-full md:w-2/3 lg:w-1/2 mx-auto">
+                <h3 className="text-xl font-semibold">
+                    Molecule Distribution by Category
+                </h3>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                    <CategoryDistChart />
+                </div>
+            </div>
+
+            <div className="w-full md:w-2/3 lg:w-1/2 mx-auto">
+                <h3 className="text-xl font-semibold">
+                    Molecule Distribution by Chirality
+                </h3>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                    <ChiralityDistChart />
+                </div>
+            </div>
+
+            <div className="w-full md:w-2/3 lg:w-1/2 mx-auto">
+                <h3 className="text-xl font-semibold">
+                    Example Result for Cluster
+                </h3>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                    <ClusterDemoChart />
+                </div>
+            </div>
         </div>
     );
 };
