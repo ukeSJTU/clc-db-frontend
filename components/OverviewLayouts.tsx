e5bfd897bbcc5aad4201c757d0eff043acff2cb4
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-22 23:55:27 +0800

    fix(ui): adjust number of cards in a row as responsive design

--- components/OverviewLayouts.tsx
+++ components/OverviewLayouts.tsx
@@ -207,7 +207,7 @@ const MoleculeCard = (molecule: MoleculeProps) => {
 // This component displays the molecules in a grid layout, each is a card
 const MolecularGridLayout = ({ molecules }: MoleculesProps) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6 mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-6 gap-6 mx-auto p-4">
             {molecules.map((molecule: MoleculeProps, index: number) => (
                 <MoleculeCard key={index} {...molecule} />
             ))}
