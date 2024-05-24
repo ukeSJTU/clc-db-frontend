c36e0de7c5068188f6f0146b440ba724dbde6abe
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-24 11:08:35 +0800

    feat: :sparkles: add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+const ContactPage = () => {
+    return (
+        <div>
+            If you have any question, please contact: Prof. Yang Yang （加粗）
+            Email: yangyang@cs.sjtu.edu.cn Department of Computer Science and
+            Engineering, and Key Laboratory of Shanghai Education Commission for
+            Intelligent Interaction and Cognitive Engineering, Shanghai Jiao
+            Tong University 800 Dongchuan Road, Shanghai 200240, China Gufeng Yu
+            （加粗） Email: jm5820zz@sjtu.edu.cn Welcome to visit our group
+            website! 网址是https://compbio.sjtu.edu.cn （变色加粗）
+        </div>
+    );
+};
+
+export default ContactPage;
